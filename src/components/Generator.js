8c9215bf05491990ba5056878920815467c713cc
SunL0w~ <5unl0w@proton.me>
2023-04-18 18:45:40 +0200

    Add Step2 Generator

--- src/components/Generator.js
+++ src/components/Generator.js
@@ -6,6 +6,11 @@ function Generator() {
   // États pour chaque champ de saisie
   const [brandKeyword, setBrandKeyword] = useState("");
   const [brandDescription, setBrandDescription] = useState("");
+  const [brandMaxLength, setBrandMaxLength] = useState(8);
+  const [brandMinLength, setBrandMinLength] = useState(7);
+  const [brandNbrSyllables, setBrandNbrSyllables] = useState(2);
+  const [brandWords, setBrandWords] = useState("");
+  const [brandLanguage, setBrandLanguage] = useState("English");
   // autres états
 
   // États pour gérer l'affichage des boîtes de dialogue
@@ -34,6 +39,11 @@ function Generator() {
       const response = await axios.post("https://the-backend-api.com/generate", {
         brandKeyword,
         brandDescription,
+        brandMaxLength,
+        brandMinLength,
+        brandNbrSyllables,
+        brandWords,
+        brandLanguage,
         // autres paramètres
       });
   // Traitez la réponse et affichez les résultats
@@ -66,7 +76,60 @@ function Generator() {
             </button>
           </div>
         );
-      // case 2, 3, 4, etc. pour les autres boîtes de dialogue
+        case 2:
+          return (
+            <div className="dialog-box">
+              <label>
+                Enter maximum length of the company name:
+                <input
+                  className="input-field"
+                  type="number"
+                  value={brandMaxLength}
+                  onChange={(e) => setBrandMaxLength(e.target.value)}
+                />
+              </label>
+              <label>
+                Enter minimum length of the company name:
+                <input
+                  className="input-field"
+                  type="number"
+                  value={brandMinLength}
+                  onChange={(e) => setBrandMinLength(e.target.value)}
+                />
+              </label>
+              <label>
+                Enter number of syllables in the name:
+                <input
+                  className="input-field"
+                  type="number"
+                  value={brandNbrSyllables}
+                  onChange={(e) => setBrandNbrSyllables(e.target.value)}
+                />
+              </label>
+              <label>
+                Enter type of words you want to include (e.g. nouns, adjectives, verbs, etc):
+                <input
+                  className="input-field"
+                  type="text"
+                  value={brandWords}
+                  onChange={(e) => setBrandWords(e.target.value)}
+                />
+              </label>
+              <label>
+                Enter language in which you want to generate the name:
+                <input
+                  className="input-field"
+                  type="text"
+                  value={brandLanguage}
+                  onChange={(e) => setBrandLanguage(e.target.value)}
+                />
+              </label>
+              <button className="next-button" onClick={handleNext}>
+                Next
+              </button>
+            </div>
+          );        
+      // case 3, 4, etc. pour les autres boîtes de dialogue
       case 6:
         return (
           <div className="dialog-box">
