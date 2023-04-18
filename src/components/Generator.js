642c29239cee7684d400512120b85d130e7409f7
SunL0w~ <5unl0w@proton.me>
2023-04-18 19:04:55 +0200

    Add Step3 Generator

--- src/components/Generator.js
+++ src/components/Generator.js
@@ -11,6 +11,8 @@ function Generator() {
   const [brandNbrSyllables, setBrandNbrSyllables] = useState(2);
   const [brandWords, setBrandWords] = useState("");
   const [brandLanguage, setBrandLanguage] = useState("English");
+  const [selectedChoices, setSelectedChoices] = useState([]);
+  const [brandStyle, setBrandStyle] = useState([]);
   // autres états
 
   // États pour gérer l'affichage des boîtes de dialogue
@@ -19,18 +21,35 @@ function Generator() {
   // Gestion du statut des erreurs
   const [hasError, setHasError] = useState(false);
 
-  // Gestionnaires d'événements pour les boutons "Next"
   const handleNext = () => {
-    if (!brandKeyword) {
-      // mettre le tour du champ en rouge si rien n'est entré
-      setHasError(true);
+    if (step === 1) {
+      if (!brandKeyword) {
+        // mettre le tour du champ en rouge si rien n'est entré
+        setHasError(true);
+      } else {
+        // L'utilisateur à bien entré quelque-chose dans le champ, pas d'erreur
+        setHasError(false);
+        // enregistrer les paramètres dans Redux
+        setStep(step + 1);
+      }
+    } else if (step === 3) {
+      if (selectedChoices.length === 0) {
+        // mettre le fond du bouton en rouge si aucun choix n'est sélectionné
+        setHasError(true);
+      } else {
+        // L'utilisateur a bien entré quelque chose, pas d'erreur
+        setHasError(false);
+        // Mettre à jour brand_style avec les choix sélectionnés
+        setBrandStyle(selectedChoices);
+        // enregistrer les paramètres dans Redux
+        setStep(step + 1);
+      }
     } else {
-      // L'utilisateur à bien entré quelque-chose dans le champ, pas d'erreur
-      setHasError(false);
       // enregistrer les paramètres dans Redux
       setStep(step + 1);
     }
   };
+  
 
   // Gestionnaire d'événements pour le bouton "Generate"
   const handleGenerate = async () => {
@@ -44,6 +63,7 @@ function Generator() {
         brandNbrSyllables,
         brandWords,
         brandLanguage,
+        brandStyle,
         // autres paramètres
       });
   // Traitez la réponse et affichez les résultats
@@ -51,6 +71,15 @@ function Generator() {
       // Gérez les erreurs
     }
   };
+
+  // Gestion des choix de l'utilisateur pour les checkbox
+  const handleChoiceChange = (choice) => {
+    if (selectedChoices.includes(choice)) {
+      setSelectedChoices(selectedChoices.filter((item) => item !== choice));
+    } else {
+      setSelectedChoices([...selectedChoices, choice]);
+    }
+  };  
 
   const renderStep = (currentStep) => {
     switch (currentStep) {
@@ -128,7 +157,53 @@ function Generator() {
                 Next
               </button>
             </div>
-          );        
+          );
+          case 3:
+            const choices = [
+              "Vintage",
+              "Artisanal",
+              "Contemporain",
+              "Moderne classique",
+              "Minimaliste japonais",
+              "Shabby chic",
+              "Bohème",
+              "Country chic",
+              "Mid-century moderne",
+              "Hollywood regency",
+              "Glamour",
+              "Rétro-futuriste",
+              "Cyberpunk",
+              "Steampunk",
+              "Punk rock",
+              "Gothique",
+              "Hipster",
+              "Skater",
+              "Surfer",
+              "Urbain",
+            ];
+          
+            return (
+              <div className="dialog-box">
+                <div className="choices-container">
+                  {choices.map((choice, index) => (
+                    <label key={index} className="choice">
+                      <input
+                        type="checkbox"
+                        checked={selectedChoices.includes(choice)}
+                        onChange={() => handleChoiceChange(choice)}
+                      />
+                      {choice}
+                    </label>
+                  ))}
+                </div>
+                <button
+                  className={`next-button${selectedChoices.length === 0 ? " error" : ""}`}
+                  onClick={handleNext}
+                >
+                  Next
+                </button>
+              </div>
+            );                  
       // case 3, 4, etc. pour les autres boîtes de dialogue
       case 6:
         return (
